440cf319974c2444714709be5707afb490d09a22
A <69404597+agsalonso@users.noreply.github.com>
2020-11-23 13:05:14 -0300

    Archivo corregido final

--- candado.py
+++ candado.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import random 
-
 import matplotlib.pyplot as plt
+
 """Se define de forma aleatoria una clave para un candado de cuatro digitos enteros y positivos."""
 def definirClave(): 
     
     clave = str(random.randint(0000,9999)).zfill(4)
    
     return clave
+
 
 """ Pre: se recibe la clave de un candado y se compara con una posible contraseña hasta dar con la correcta.
     Aclaración: la clave hace referencia a los dígitos correctos con los cuales se abre el candado.
@@ -26,36 +27,35 @@ def realizarExperimento():
     # print(intentos)    
     return intentos
 
-"Se realizan la cantidad indicada de experimentos, en este caso, 100.000"
+
+"""Se realizan la cantidad indicada de experimentos, en este caso, 100.000"""
 def experimentos():
     
     datos = []
-    maxExperimentos = 100 #aca tiene que haber 100000
+    maxExperimentos = 100000 
     cantidadExperimentos = 0
     
-    #print(type(maxExperimentos))
-    #print(type(cantidadExperimentos))
     while(cantidadExperimentos < maxExperimentos):
         intento = realizarExperimento()
         cantidadExperimentos+=1
-       # print(cantidadExperimentos)
         datos.append(intento)
-       # print(datos)
+       
     return datos
         
-#experimentos() 
 
-
+"""Se realiza un histograma con la cantidad de intentos que fueron necesarios para descubrir la clave en cada experimento realizado."""
 def histogram(datos):
  
     
-    num_bins = 10000 #cantidad de rangos??
-    plt.hist(datos, num_bins, facecolor='blue', alpha=0.5)
-    plt.title('Este es un titulo')
-    plt.xlabel('Cantidad de experimentos')
-    plt.ylabel('Frecuencia')
+    num_bins = 1000 #cantidad de columnas / 0-10, 10-20, ... 
+    plt.hist(datos, num_bins, facecolor='blue', alpha=5)
+    plt.title('¿Cuántos intentos se realizan hasta que se descubre la clave?')
+    plt.xlabel('Intentos realizados')
+    plt.ylabel('Cantidad de experimentos')
 
     plt.show()
 
 
+
+"""Ejecución del programa"""
 histogram(experimentos())
